e9a575d1e142c567206d8f2b02a6cbefe75d6b4e
ThienAnDy <vaper2212@gmail.com>
2025-03-01 13:19:23 +0700

    setup basic

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from '@heroui/react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-amber-300'>Tesst</h1>
+      <Button>Check</Button>
+    </>
+  )
+}
+
+export default App
